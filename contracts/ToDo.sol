83331353677ad72926041cfc1b2284f0fd100b54
Osuolale1 <ayoolaoluwafemi2018@gmail.com>
2024-02-13 15:34:06 +0100

    test

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -18,4 +18,36 @@ List[] public todoList;
     });
   todoList.push(newTodo);
  }
+ 
+ function toggleIsDone(uint index) external {
+    require(index < todoList.length, "invalid index");
+    todoList[index].isDone = !todoList[index].isDone;
+ }
+
+ function updateTitle(uint index, string memory newTitle) external {
+   require(index < todoList.length, "invalid index");
+   todoList[index].title = newTitle;
+ }
+
+  function updateDescription(uint index, string memory newDescription) external {
+   require(index < todoList.length, "invalid index");
+   todoList[index].description = newDescription;
+ }
+
+  function updateIsDone(uint index, bool newIsDone) external {
+   require(index < todoList.length, "invalid index");
+   todoList[index].isDone = newIsDone;
+ }
+
+ function deleteTodo(uint256 index) external {
+        require(index < todoList.length, "Invalid index");
+
+        todoList[index] = todoList[todoList.length - 1];
+
+        todoList.pop();
+}
+
+function getTodo() external view returns(List[] memory) {
+    return todoList;
+}
 }
